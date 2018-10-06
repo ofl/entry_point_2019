602abdc433d41a64c944b17e2919915d56faa182
ofl <ofl@covered.jp>
2018-10-06 16:39:59 +0900

    [#17] rubocopエラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root :to => 'users#index'
+  root to: 'users#index'
   resources :user_sessions
   resources :users
 
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
 end
