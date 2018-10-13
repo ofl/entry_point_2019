40908683391b600863fc8d243e7c311e829b3163
ofl <ofl@covered.jp>
2018-10-13 16:05:05 +0900

    [#28] npx create-laravel-mix

---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath('public')
+    .js('assets/js/app.js', 'js')
+    .sass('assets/sass/app.scss', 'css');
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+    mix.sourceMaps()
+        .webpackConfig({devtool: 'source-map'});
+}
