227e3d8b6cac4bec8470b5a655afb2694232d8dc
ofl <ofl@covered.jp>
2018-10-27 15:32:06 +0900

    [#37] Articleのリストをvuetifyのコンポーネントで表示する

---
+++ assets/js/components/pages/ArticleList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app id="inspire" v-cloak>
+    <my-navigation></my-navigation>
+    <my-toolbar :title="toolbarTitle"></my-toolbar>
+    <my-content :articles="articles"></my-content>
+    <my-footer></my-footer>
+  </v-app>
+</template>
+
+<style>
+  [v-cloak] {
+    /* display: none; */
+  }
+</style>
+
+<script>
+  import Navigation from '../templates/Navigation.vue';
+  import Toolbar from '../templates/Toolbar.vue';
+  import Footer from '../templates/Footer.vue';
+  import Content from '../templates/contents/ArticleList.vue';
+
+  export default {
+    name: 'article-list',
+    components: {
+      'my-navigation': Navigation,
+      'my-toolbar': Toolbar,
+      'my-content': Content,
+      'my-footer': Footer
+    },
+    props: {
+      articles: {
+        type: Array
+      }
+    },
+    data: () => ({
+      toolbarTitle: 'Articles',
+    })
+    // ページ全体に及ぶロジックを記述
+  };
+</script>
