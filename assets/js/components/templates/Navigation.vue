3ad15358c45c6389a6393fceab7dc45d74d19e78
ofl <ofl@covered.jp>
2018-10-28 13:38:57 +0900

    [#40] Navigationメニューから移動可能にする

--- assets/js/components/templates/Navigation.vue
+++ assets/js/components/templates/Navigation.vue
@@ -5,7 +5,7 @@
     app
   >
     <v-list dense>
-      <v-list-tile @click="">
+      <v-list-tile @click="onClickMenu('/')">
         <v-list-tile-action>
           <v-icon>home</v-icon>
         </v-list-tile-action>
@@ -13,12 +13,12 @@
           <v-list-tile-title>Home</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
-      <v-list-tile @click="">
+      <v-list-tile @click="onClickMenu('/vue_articles')">
         <v-list-tile-action>
           <v-icon>contact_mail</v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
-          <v-list-tile-title>Contact</v-list-tile-title>
+          <v-list-tile-title>Articles</v-list-tile-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
@@ -29,6 +29,11 @@
   export default {
     data: () => ({
       drawer: null
-    })
+    }),
+    methods: {
+      onClickMenu(path) {
+        location.href = path;
+      }
+    }
   }
 </script>
