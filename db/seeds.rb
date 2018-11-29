30a81b45ed9b4649bc8b21633f33acdc23797133
ofl <ofl@covered.jp>
2018-11-29 18:34:20 +0900

    [#75] seedデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,15 @@ def create(*args)
   FactoryBot.create(*args)
 end
 
-def create_user(name = Faker::Internet.user_name.slice(0...15))
-  user = create(:user, name: name, email: Faker::Internet.email)
+def create_user(name: Faker::Internet.user_name.slice(0...15), email: Faker::Internet.email)
+  user = create(:user, name: name, email: email)
   user
 end
 
 users = []
 
 unless User.exists?
-  users << create_user('testuser')
+  users << create_user(name: 'testuser', email: 'test@example.com')
 
   10.times do
     users << create_user
@@ -37,6 +37,10 @@ unless Article.exists?
   articles.each do |article|
     rand(3).times do
       create(:comment, user: users.sample, article: article)
+    end
+
+    rand(3).times do
+      create(:like, user: users.sample, article: article)
     end
   end
 end
