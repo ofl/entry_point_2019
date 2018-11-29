7f5d92f598382c87bee9c576fbcb19478d2f0e48
ofl <ofl@covered.jp>
2018-11-29 18:16:21 +0900

    [#75] User、Articleにカラムを追加

--- db/migrate/20181006064416_create_users.rb
+++ db/migrate/20181006064416_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
+      t.string :name, null: false, limit: 50
+      t.string :email, null: false, limit: 100
+      t.string :avatar, limit: 100
       t.string :crypted_password, null: false
       t.string :salt, null: false
 
