276f0bb5b77f9c22a1ce4bec41d038616def4edd
ofl <ofl@covered.jp>
2018-12-03 13:20:42 +0900

    [#110]SpaArticlesControllerの作成

---
+++ app/controllers/spa_articles_controller.rb
@@ -0,0 +1,19 @@
+class SpaArticlesController < ApplicationController
+  skip_before_action :require_login, only: [:index, :show]
+
+  # GET /spa_articles
+  def index
+  end
+
+  # GET /spa_articles/1
+  def show
+  end
+
+  # GET /spa_articles/new
+  def new
+  end
+
+  # GET /spa_articles/1/edit
+  def edit
+  end
+end
