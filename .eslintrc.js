5bbe652e8f97603d90b1a8f024a55376ea5d37c4
ofl <ofl@covered.jp>
2018-12-15 17:20:47 +0900

    [#113] add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "amd": true
+    },
+    "extends": [
+          "eslint:recommended",
+          "plugin:vue/recommended",
+          "plugin:prettier/recommended"
+        ]
+};
