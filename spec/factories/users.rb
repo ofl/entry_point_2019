339d033b0cc43e08219cccc20560f2be68a5cbae
ofl <ofl@covered.jp>
2019-03-23 17:37:16 +0900

    [#187]ローカルアップロード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,6 +18,9 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
-    avatar { '' }
+
+    trait :with_avatar do
+      avatar { File.open(Rails.root.join('spec', 'fixtures', 'files', 'youngman_31.png')) }
+    end
   end
 end
