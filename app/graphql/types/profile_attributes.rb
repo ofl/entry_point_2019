f0633a6c04404b86c8f87b2110ee456643b7d942
ofl <ofl@covered.jp>
2019-03-23 15:59:22 +0900

    [#78] avatarをアップロード

--- app/graphql/types/profile_attributes.rb
+++ app/graphql/types/profile_attributes.rb
@@ -1,6 +1,7 @@
 class Types::ProfileAttributes < Types::BaseInputObject
   description 'Attributes for profile'
 
-  argument :name, String, 'Name for the post profile', required: true
-  argument :email, String, 'Email for the post profile', required: true
+  argument :name, String, 'Name for the update profile', required: true
+  argument :email, String, 'Email for the update profile', required: true
+  argument :avatar, ApolloUploadServer::Upload, 'Avatar Image File for the update profile', required: false
 end
