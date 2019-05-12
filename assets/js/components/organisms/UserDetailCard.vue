a0a888bfcd20fe7ca76d98e6573c8117bc0a2a9d
ofl <ofl@covered.jp>
2019-05-12 15:38:49 +0900

    [#205]Userの詳細表示(Vue側)

---
+++ assets/js/components/organisms/UserDetailCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="avatarUrl" alt="Placeholder image">
+      </figure>
+    </div>
+
+    <div class="card-content">
+      <div class="content">{{ user.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserDetailCard",
+
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    currentUser: {
+      type: Object
+    }
+  },
+
+  computed: {
+    isLoggedIn() {
+      return !!this.currentUser;
+    },
+    avatarUrl() {
+      return !!this.user.avatarUrl
+        ? this.user.avatarUrl
+        : "../images/300x300.png";
+    }
+  }
+};
+</script>
